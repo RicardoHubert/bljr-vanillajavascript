2f9f1cd8b2ca2e1fd4b128f3122f0ca6cb4f0a31
RicardoHubert <32365031+RicardoHubert@users.noreply.github.com>
2023-05-21 01:00:48 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  hobbies: ["music", "movies", "sports"],
+  address: {
+    street: "50 main st",
+    city: "Boston",
+    state: "MA"
+  }
+};
+// console.log(person.firstName, person.lastName);
+
+const {
+  fitstName,
+  lastName,
+  address: { city }
+} = person;
+
+console.log(city);
